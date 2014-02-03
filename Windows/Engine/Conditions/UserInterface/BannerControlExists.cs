81b6741812f84535a1ca360ad93005f39a16acb1
Brent Moberly <bmoberly@iu.edu>
2014-02-03 14:44:09 -0500

    fixed issue w/ UserInterface.BannerControlExists
    UserInterface.BannerControlExists was always returning false.  Updated
    code to fix issue.
    Increased version to 14.1.2.0

--- Windows/Engine/Conditions/UserInterface/BannerControlExists.cs
+++ Windows/Engine/Conditions/UserInterface/BannerControlExists.cs
@@ -1,8 +1,6 @@
 ﻿using System.Linq;
-using System.Windows;
 using Org.InCommon.InCert.Engine.Results.Misc;
 using Org.InCommon.InCert.Engine.Engines;
-using Org.InCommon.InCert.Engine.UserInterface.Dialogs.Instances.Pages;
 using Org.InCommon.InCert.Engine.Utilities;
 
 namespace Org.InCommon.InCert.Engine.Conditions.UserInterface
@@ -21,15 +19,10 @@ namespace Org.InCommon.InCert.Engine.Conditions.UserInterface
             if (dialog == null)
                 return new BooleanReason(false, "Cannot retrieve dialog for key {0}", _dialog);
 
-            var page = dialog.ContentModel.Content as BannerPage;
-            if (page == null)
-                return new BooleanReason(false, "Cannot retrieve banner page from dialog.");
-
-            var instances = page.GetNamedElements<FrameworkElement>(_key);
-            if (!instances.Any())
-                return new BooleanReason(false, "no instances of {0} found", _key);
-
-            return new BooleanReason(true, "at least one instance of {0} found", _key);
+            var instances = dialog.GetModelsByKey(_key);
+            return !instances.Any() 
+                ? new BooleanReason(false, "no instances of {0} found", _key) 
+                : new BooleanReason(true, "at least one instance of {0} found", _key);
         }
 
         public override bool IsInitialized()
