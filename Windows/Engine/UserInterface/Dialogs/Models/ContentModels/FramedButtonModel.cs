947c5cb260e19be61088166b97c8cf65a3683fcc
Brent Moberly <bmoberly@iu.edu>
2014-06-06 17:35:30 -0400

    removing unused includes from framedbutton

--- Windows/Engine/UserInterface/Dialogs/Models/ContentModels/FramedButtonModel.cs
+++ Windows/Engine/UserInterface/Dialogs/Models/ContentModels/FramedButtonModel.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Org.InCommon.InCert.Engine.Engines;
