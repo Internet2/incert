1578632aff9a3e4ef550ec02a2f543da1f71d83f
Brent Moberly <bmoberly@iu.edu>
2015-01-13 13:32:40 -0500

    adding showhtmlbannermodal task

---
+++ Windows/Engine/Tasks/UserInterface/ShowHtmlBannerModal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Org.InCommon.InCert.Engine.Engines;
+using Org.InCommon.InCert.Engine.Importables;
+using Org.InCommon.InCert.Engine.Results;
+using Org.InCommon.InCert.Engine.Results.Errors.UserInterface;
+using Org.InCommon.InCert.Engine.UserInterface.Dialogs.Models.DialogModels;
+
+namespace Org.InCommon.InCert.Engine.Tasks.UserInterface
+{
+    class ShowHtmlBannerModal:AbstractTask
+    {
+        public ShowHtmlBannerModal(IEngine engine) : base(engine)
+        {
+        }
+
+        [PropertyAllowedFromXml]
+        public string Banner
+        {
+            get { return GetDynamicValue(); }
+            set { SetDynamicValue(value); }
+        }
+
+        [PropertyAllowedFromXml]
+        public string Dialog
+        {
+            get { return GetDynamicValue(); }
+            set { SetDynamicValue(value); }
+        }
+
+        public override IResult Execute(IResult previousResults)
+        {
+            var dialog = DialogsManager.GetDialog<HtmlDialogModel>(Dialog);
+            if (dialog == null)
+                return new DialogInstanceNotFound { Dialog = Dialog };
+
+            DialogsManager.ActiveDialogKey = Dialog;
+            return dialog.ShowPage(Banner);
+        }
+
+        public override string GetFriendlyName()
+        {
+            return string.Format("Show html banner {0} in model dialog {1}", Banner, Dialog);
+        }
+    }
+}
