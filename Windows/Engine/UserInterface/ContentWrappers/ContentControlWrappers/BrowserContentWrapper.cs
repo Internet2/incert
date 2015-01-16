0f843241a732dc7817f78db4680533acec669a37
Brent Moberly <bmoberly@iu.edu>
2015-01-16 14:01:22 -0500

    adding browser content wrapper

---
+++ Windows/Engine/UserInterface/ContentWrappers/ContentControlWrappers/BrowserContentWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using Org.InCommon.InCert.Engine.Engines;
+
+namespace Org.InCommon.InCert.Engine.UserInterface.ContentWrappers.ContentControlWrappers
+{
+    class BrowserContentWrapper:AbstractContentWrapper
+    {
+        public string Url { get; set; }
+        
+        public BrowserContentWrapper(IEngine engine) : base(engine)
+        {
+            
+        }
+    }
+}
