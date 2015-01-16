baa807917ca59189596cf3c8823846e9859e2b5e
Brent Moberly <bmoberly@iu.edu>
2015-01-16 13:58:24 -0500

    adding browser content model

---
+++ Windows/Engine/UserInterface/Dialogs/Models/ContentModels/BrowserContentModel.cs
@@ -0,0 +1,22 @@
+﻿using Org.InCommon.InCert.Engine.UserInterface.ContentWrappers.ContentControlWrappers;
+using Org.InCommon.InCert.Engine.UserInterface.Dialogs.Instances.CustomControls;
+
+namespace Org.InCommon.InCert.Engine.UserInterface.Dialogs.Models.ContentModels
+{
+    public class BrowserContentModel:AbstractContentModel
+    {
+        public BrowserContentModel(AbstractModel parentModel) : base(parentModel)
+        {
+            
+        }
+
+        public override T LoadContent<T>(AbstractContentWrapper wrapper)
+        {
+            var content = new BrowserControl {DataContext = this};
+            InitializeBindings(content);
+
+            return content as T;
+
+        }
+    }
+}
