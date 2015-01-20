63495808a96588463e70b8a9b7bac17967b0defb
Brent Moberly <bmoberly@iu.edu>
2015-01-20 16:31:51 -0500

    removing browserControl.xaml
    will just use contentModel class to instantiate browser instance, etc.

--- Windows/Engine/UserInterface/Dialogs/Models/ContentModels/BrowserContentModel.cs
+++ Windows/Engine/UserInterface/Dialogs/Models/ContentModels/BrowserContentModel.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using CefSharp;
+using CefSharp.Wpf;
+using log4net;
+using Org.InCommon.InCert.Engine.Logging;
 using Org.InCommon.InCert.Engine.UserInterface.ContentWrappers.ContentControlWrappers;
 using Org.InCommon.InCert.Engine.UserInterface.Dialogs.Instances.CustomControls;
 using Org.InCommon.InCert.Engine.UserInterface.Dialogs.Models.ScriptingModels;
@@ -7,16 +12,19 @@ namespace Org.InCommon.InCert.Engine.UserInterface.Dialogs.Models.ContentModels
 {
     public class BrowserContentModel:AbstractContentModel
     {
+        private static readonly ILog Log = Logger.Create();
+
         public BrowserContentModel(AbstractModel parentModel) : base(parentModel)
         {
+            InitializeChromium();
             
         }
 
         public override T LoadContent<T>(AbstractContentWrapper wrapper)
         {
-            var content = new BrowserControl {DataContext = this};
-            InitializeBindings(content);
-            InitializeValues(wrapper);
+            var content = new ChromiumWebBrowser();
+           /* InitializeBindings(content);
+            InitializeValues(wrapper);*/
 
             var browserWrapper = wrapper as BrowserContentWrapper;
             if (browserWrapper == null)
@@ -24,18 +32,48 @@ namespace Org.InCommon.InCert.Engine.UserInterface.Dialogs.Models.ContentModels
                 throw new InvalidCastException("Could not cast wrapper to valid type");
             }
 
-            content.SilentMode = browserWrapper.SilentMode;
-            content.Browser.ObjectForScripting = new ScriptingModel(wrapper.Engine, RootDialogModel);
-            content.Browser.Navigate(browserWrapper.Uri, null, null, "Incert: true\r\n");
-            
+           /* content.SilentMode = browserWrapper.SilentMode;
+            content.Browser.ObjectForScripting = new ScriptingModel(wrapper.Engine, RootDialogModel);*/
+            content.RegisterJsObject("engine", new ScriptingModel(wrapper.Engine, RootDialogModel));
+            content.Address = browserWrapper.Uri.AbsoluteUri;
+            //content.Browser.Navigate(browserWrapper.Uri, null, null, "Incert: true\r\n");
+            content.Width = 600;
+            content.Height = 600;
+
+            content.ConsoleMessage += ConsoleMessageHandler;
+
             Content = content;
 
             return content as T;
 
         }
 
+        private void ConsoleMessageHandler(object sender, ConsoleMessageEventArgs e)
+        {
+            Log.InfoFormat(e.Message);
+        }
+
+        private static void InitializeChromium()
+        {
+            if (Cef.IsInitialized)
+            {
+                return;
+            }
+
+            var settings = new CefSettings
+            {
+                PackLoadingDisabled = true,
+                LogSeverity =  LogSeverity.Disable
+            };
+
+            if (!Cef.Initialize(settings))
+            {
+                throw new Exception("Could not initialize Chromium browser");
+            }    
+        }
 
 
+        
         
     }
 }
