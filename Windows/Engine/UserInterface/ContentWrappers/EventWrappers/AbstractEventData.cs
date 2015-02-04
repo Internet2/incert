bdf3a7a7a0f79cd8cf287f2c2abbb0e67f9cb8a5
Brent Moberly <bmoberly@iu.edu>
2015-02-04 16:53:52 -0500

    adding rudimentary task and branch-specific event data

---
+++ Windows/Engine/UserInterface/ContentWrappers/EventWrappers/AbstractEventData.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace Org.InCommon.InCert.Engine.UserInterface.ContentWrappers.EventWrappers
+{
+    public abstract class AbstractEventData
+    {
+        public string ToJson()
+        {
+            var serializer = new DataContractJsonSerializer(GetType());
+            using (var stream = new MemoryStream())
+            {
+                serializer.WriteObject(stream, this);
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
---
+++ Windows/Engine/UserInterface/ContentWrappers/EventWrappers/TaskEventData.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+using Org.InCommon.InCert.Engine.Tasks;
+
+namespace Org.InCommon.InCert.Engine.UserInterface.ContentWrappers.EventWrappers
+{
+    [DataContract]
+    public class TaskEventData:AbstractEventData
+    {
+        public TaskEventData()
+        {
+            
+        }
+
+        public TaskEventData(ITask task)
+        {
+            Id = task.Id;
+            Message = task.UiMessage;
+        }
+        
+        [DataMember(Name = "id", Order = 1)]
+        public string Id { get; set; }
+
+        [DataMember(Name="message", Order=2)]
+        public string Message { get; set; }
+    }
+}
