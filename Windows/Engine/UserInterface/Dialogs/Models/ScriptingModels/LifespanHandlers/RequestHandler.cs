550c1e34c8a3a5c2c4ab532fea8c514346d41d14
Brent Moberly <bmoberly@iu.edu>
2015-02-25 12:29:50 -0500

    adding redirect urls to embedded resources
    If the engine sees a request for a file that matches the name of one of
    its embedded html resource files, it will redirect that request to the
    embedded file.  The intent here is to allow the engine to substitute its
    internal resources for external resources, if it has the resources
    already.  For example, pages that reference jquery against the external
    cdn would fetch jquery from the internal resource rather than the
    cdn....

--- Windows/Engine/UserInterface/Dialogs/Models/ScriptingModels/LifespanHandlers/RequestHandler.cs
+++ Windows/Engine/UserInterface/Dialogs/Models/ScriptingModels/LifespanHandlers/RequestHandler.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using CefSharp;
 using log4net;
 using Org.InCommon.InCert.Engine.Logging;
 
 namespace Org.InCommon.InCert.Engine.UserInterface.Dialogs.Models.ScriptingModels.LifespanHandlers
 {
-    class RequestHandler:IRequestHandler
+    class RequestHandler : IRequestHandler
     {
+        private readonly Dictionary<string, string> _embeddedResourceDictionary;
+
         private static readonly ILog Log = Logger.Create();
+
+        public RequestHandler()
+        {
+            _embeddedResourceDictionary = BuildEmbeddedResourceDictionary();
+        }
 
         public bool OnBeforeBrowse(IWebBrowser browser, IRequest request, bool isRedirect)
         {
@@ -30,7 +37,14 @@ namespace Org.InCommon.InCert.Engine.UserInterface.Dialogs.Models.ScriptingModel
 
         public bool OnBeforeResourceLoad(IWebBrowser browser, IRequest request, IResponse response)
         {
-           
+            var redirectUrl = GetRedirectUrl(request.Url);
+            if (!string.IsNullOrWhiteSpace(redirectUrl))
+            {
+                Log.DebugFormat("redirecting {0} to {1}", request.Url, redirectUrl);
+                response.Redirect(redirectUrl);
+                return false;
+            }
+
             Log.DebugFormat("resource load request: {0}", request.Url);
             return false;
         }
@@ -47,7 +61,56 @@ namespace Org.InCommon.InCert.Engine.UserInterface.Dialogs.Models.ScriptingModel
 
         public void OnRenderProcessTerminated(IWebBrowser browser, CefTerminationStatus status)
         {
+
+        }
+
+        private static Dictionary<string, string> BuildEmbeddedResourceDictionary()
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            AddPathsToDictonary(dictionary, "Org.InCommon.InCert.Engine.Content.Html.Fonts", "resource://html/fonts/");
+            AddPathsToDictonary(dictionary, "Org.InCommon.InCert.Engine.Content.Html.Css", "resource://html/css/");
+            AddPathsToDictonary(dictionary, "Org.InCommon.InCert.Engine.Content.Html.Scripts", "resource://html/scripts/");
+
+            return dictionary;
+        }
+
+        private static void AddPathsToDictonary(IDictionary<string, string> dictionary, string manifestPrefix, string urlPrefix)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resources = assembly.GetManifestResourceNames()
+                .Where(p => p.StartsWith(manifestPrefix));
+
+            foreach (var resource in resources.Select(r=>RemoveManifestPrefix(r, manifestPrefix)))
+            {
+                dictionary[resource.ToLowerInvariant()] = urlPrefix + resource;
+            }
+        }
+
+        private static string RemoveManifestPrefix(string value, string prefix)
+        {
+            value = value.Replace(prefix, "");
+            return value.TrimStart('.');
+        }
+
+        private string GetRedirectUrl(string url)
+        {
+            var target = Path.GetFileName(url);
+            if (string.IsNullOrWhiteSpace(target))
+            {
+                return string.Empty;
+            }
             
+            target = target.ToLowerInvariant();
+            if (!_embeddedResourceDictionary.ContainsKey(target))
+            {
+                return string.Empty;
+            }
+
+            var redirectUrl = _embeddedResourceDictionary[target];
+            return redirectUrl.Equals(url, StringComparison.InvariantCultureIgnoreCase) 
+                ? string.Empty 
+                : redirectUrl;
         }
     }
 }
