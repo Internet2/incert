747e3137805127cd565afcee504fe27f0ec66599
Brent Moberly <bmoberly@iu.edu>
2015-03-13 18:05:04 -0400

    adding CouldNotLoadHtmlContentError

---
+++ Windows/Engine/Results/Errors/UserInterface/CouldNotLoadHtmlContent.cs
@@ -0,0 +1,12 @@
+﻿using Org.InCommon.InCert.Engine.Extensions;
+
+namespace Org.InCommon.InCert.Engine.Results.Errors.UserInterface
+{
+    internal class CouldNotLoadHtmlContent : ErrorResult
+    {
+        [ResultExtensions.IncludeInErrorDetails]
+        public string Url { get; set; }
+
+        public bool IsExternalUrl { get; set; }
+    }
+}
