5f26eaba74ae406ab29e542756486667881e95c6
Brent Moberly <bmoberly@iu.edu>
2015-03-23 15:00:46 -0400

    extracting ellipsis to stand-alone class

---
+++ Windows/Engine/Content/Html/Scripts/ellipsis.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    function init() {
+        $(".ellipsis").each(function () {
+            var instance = new ellipses(this);
+            $(this).data("ellipsis-instance", instance);
+            instance.start();
+        });
+    }
+
+    $(document).ready(function () {
+        init();
+    });
+
+    function ellipses(element) {
+        return {
+            value: ["", ".", "..", "..."],
+            count: 0,
+            run: false,
+            timer: null,
+            start: function () {
+                console.log("starting");
+                var t = this;
+                this.run = true;
+                this.timer = setInterval(function () {
+                    if (t.run) {
+                        $(element).html(t.value[t.count % t.value.length]).text();
+                        t.count++;
+                    }
+                }, 500);
+            },
+            stop: function () {
+                console.log("stopping");
+                this.run = false;
+                clearInterval(this.timer);
+                this.count = 0;
+                $(element).text("");
+            }
+        }
+    }
+})();
--- Windows/Engine/Content/Html/Scripts/inCert.js
+++ Windows/Engine/Content/Html/Scripts/inCert.js
@@ -510,31 +510,6 @@
 
     configureForInBrowserTesting();
 
-    ellipsis = {
-        'value': ['', '.', '..', '...'],
-        'count': 0,
-        'run': false,
-        'timer': null,
-        'element': '.ellipsis',
-        'start': function () {
-            var t = this;
-            this.run = true;
-            this.timer = setInterval(function () {
-                if (t.run) {
-                    $(t.element).html(t.value[t.count % t.value.length]).text();
-                    t.count++;
-                }
-            }, 500);
-        },
-        'stop': function () {
-            this.run = false;
-            clearInterval(this.timer);
-            this.count = 0;
-        }
-    }
-
-    ellipsis.start();
-
     $(document).ready(function () {
         globalInitialize();
     });
