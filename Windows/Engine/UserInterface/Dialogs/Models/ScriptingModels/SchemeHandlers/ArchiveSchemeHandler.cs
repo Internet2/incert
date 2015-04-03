4328d568889985032e5693f97f5ac8502b93a9fa
Brent Moberly <bmoberly@iu.edu>
2015-04-03 16:17:33 -0400

    adding error handling, logging to archive scheme handler

--- Windows/Engine/UserInterface/Dialogs/Models/ScriptingModels/SchemeHandlers/ArchiveSchemeHandler.cs
+++ Windows/Engine/UserInterface/Dialogs/Models/ScriptingModels/SchemeHandlers/ArchiveSchemeHandler.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using CefSharp;
+using log4net;
+using Org.InCommon.InCert.Engine.Logging;
 using Org.InCommon.InCert.Engine.Utilities;
 
 // adapted from http://thechriskent.com/2014/05/12/use-embedded-resources-in-cefsharp/
@@ -26,38 +28,55 @@ namespace Org.InCommon.InCert.Engine.UserInterface.Dialogs.Models.ScriptingModel
 
     public class ArchiveSchemeHandler : AbstractSchemeHandler
     {
+        private static readonly ILog Log = Logger.Create();
+
         public override bool ProcessRequestAsync(IRequest request, ISchemeHandlerResponse response, OnRequestCompletedHandler requestCompletedCallback)
         {
-            var uri = new Uri(request.Url);
-            
-            var archivePath = FindArchive(uri.Authority);
-            if (string.IsNullOrWhiteSpace(archivePath))
+            try
             {
-                response.StatusCode = (int)HttpStatusCode.NotFound;
+                var uri = new Uri(request.Url);
+
+                var archivePath = FindArchive(uri.Authority);
+                if (string.IsNullOrWhiteSpace(archivePath))
+                {
+                    Log.WarnFormat("Cannot load content from archive url {0}: archive not found", request.Url);
+                    response.StatusCode = (int) HttpStatusCode.NotFound;
+                    return false;
+                }
+
+                var fileName = ResolvePath(CabArchiveUtilities.GetFilesInArchive(archivePath), uri.Segments.Last());
+                if (string.IsNullOrWhiteSpace(fileName))
+                {
+                    Log.WarnFormat("Cannot load content from archive url {0}: resource not found in archive ({1})", request.Url, uri.Segments.Last());
+                    response.StatusCode = (int) HttpStatusCode.NotFound;
+                    return false;
+                }
+
+                var contentBytes = CabArchiveUtilities.ExtractFile(archivePath, fileName);
+                if (contentBytes == null)
+                {
+                    Log.WarnFormat("Cannot load content from archive url {0}: could not load resource", request.Url);
+                    response.StatusCode = (int) HttpStatusCode.NotFound;
+                    return false;
+                }
+
+                var stream = new MemoryStream(contentBytes);
+                response.MimeType = GetMimeType(fileName);
+                response.ResponseStream = stream;
+                response.StatusCode = (int) HttpStatusCode.OK;
+                AddAccessControlHeader(response);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.WarnFormat("An exception occurred trying to load resource from archive: {0}",e);
+                response.StatusCode = (int) HttpStatusCode.InternalServerError;
                 return false;
             }
-
-            var fileName = ResolvePath(CabArchiveUtilities.GetFilesInArchive(archivePath), uri.Segments.Last());
-            if (string.IsNullOrWhiteSpace(fileName))
+            finally
             {
-                response.StatusCode = (int)HttpStatusCode.NotFound;
-                return false;
+                requestCompletedCallback();
             }
-
-            var contentBytes = CabArchiveUtilities.ExtractFile(archivePath, fileName);
-            if (contentBytes == null)
-            {
-                response.StatusCode = (int)HttpStatusCode.NotFound;
-                return false;
-            }
-
-            var stream = new MemoryStream(contentBytes);
-            response.MimeType = GetMimeType(fileName);
-            response.ResponseStream = stream;
-            response.StatusCode = (int)HttpStatusCode.OK;
-            AddAccessControlHeader(response);
-            requestCompletedCallback();
-            return true;
         }
 
         private static string FindArchive(string archiveName)
